22eff30a4f68c3bf5a8418c8a95359261e2244b1
duonghovo <duonghovo@gmail.com>
2023-07-31 21:11:58 +0700

    boost map

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import { layer, Map, Layers } from "react-openlayers";
 import "./App.css";
+import Menu from "./components/menu/Menu";
 function App() {
   return (
     <div className="App">
-      <Map
-        view={{
-          projection: "EPSG:4326",
-          center: [107.84641987555096, 15.557018118480753],
-          zoom: 5,
-        }}
-        style={{ height: "100vh", width: "100vw" }}
-      >
-        <Layers>
-          <layer.Tile></layer.Tile>
-        </Layers>
-      </Map>
+      <div style={{ display: "flex" }}>
+        <Menu />
+        <Map
+          view={{
+            projection: "EPSG:4326",
+            center: [107.84641987555096, 15.557018118480753],
+            zoom: 5.5,
+          }}
+          style={{ height: "100vh", width: "80vw" }}
+        >
+          <Layers>
+            <layer.Tile></layer.Tile>
+          </Layers>
+        </Map>
+      </div>
     </div>
   );
 }
---
+++ src/components/menu/Menu.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Menu } from "antd";
+
+const { SubMenu } = Menu;
+
+function App() {
+  return (
+    <Menu mode="inline" style={{ width: 256 }}>
+      <SubMenu key="sub1" title="Chọn vệ tinh">
+        <Menu.Item key="1">Lựa chọn 1</Menu.Item>
+        <Menu.Item key="2">Lựa chọn 2</Menu.Item>
+        <SubMenu key="sub1-2" title="Chọn chi tiết vị tinh  ">
+          <Menu.Item key="3">Lựa chọn 3</Menu.Item>
+          <Menu.Item key="4">Lựa chọn 4</Menu.Item>
+        </SubMenu>
+      </SubMenu>
+      <SubMenu key="sub2" title="Chọn khu vực">
+        <Menu.Item key="5">Lựa chọn 5</Menu.Item>
+        <Menu.Item key="6">Lựa chọn 6</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+}
+
+export default App;
