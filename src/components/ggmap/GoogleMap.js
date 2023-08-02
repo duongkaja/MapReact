efe4134373fb5c00314991c79bd20a7061f31be4
tours <minhquando59@gmail.com>
2023-08-02 15:15:43 +0700

    GPT

--- src/components/ggmap/GoogleMap.js
+++ src/components/ggmap/GoogleMap.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Map from "ol/Map.js";
 import View from "ol/View.js";
 import OLGoogleMaps from "olgm/OLGoogleMaps.js";
@@ -10,26 +10,32 @@ import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
 import { Draw, Modify, Snap } from "ol/interaction.js";
 
 const GoogleMap = () => {
-  const osmSource = new OSM();
+  const mapRef = useRef(null);
+
   useEffect(() => {
-    const map = new Map({
-      // use OL3-Google-Maps recommended default interactions
-      interactions: defaultInteractions(),
-      layers: [
-        new TileLayer({
-          source: osmSource, //*  An OpenStreetMap layer providing the base map tiles
+    if (!mapRef.current) {
+      const osmSource = new OSM();
+      const map = new Map({
+        // use OL3-Google-Maps recommended default interactions
+        interactions: defaultInteractions(),
+        layers: [
+          new TileLayer({
+            source: osmSource, //*  An OpenStreetMap layer providing the base map tiles
+          }),
+        ],
+        target: "map",
+        view: new View({
+          center: transform(
+            [107.84641987555096, 15.557018118480753],
+            "EPSG:4326",
+            "EPSG:3857"
+          ),
+          zoom: 5.8,
         }),
-      ],
-      target: "map",
-      view: new View({
-        center: transform(
-          [107.84641987555096, 15.557018118480753],
-          "EPSG:4326",
-          "EPSG:3857"
-        ),
-        zoom: 5.8,
-      }),
-    });
+      });
+
+      mapRef.current = map;
+    }
   }, []);
 
   return <div id="map" style={{ height: "100vh", width: "80vw" }} />;
