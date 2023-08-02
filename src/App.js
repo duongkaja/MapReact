7bd5e167b72c79ff958318e86f7f595e25f10432
duonghovo <duonghovo@gmail.com>
2023-08-02 21:58:43 +0700

    map chi so

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from "react";
 // import { layer, Map as OLMap, Layers } from "react-openlayers";
 import "./App.css";
 import Menu from "./components/menu/Menu";
-import MapWrapper from "./components/OpenLayers/MapWrapper";
+// import MapWrapper from "./components/OpenLayers/MapWrapper";
 import GoogleMap from "./components/ggmap/GoogleMap";
 function App() {
   return (
     <div className="App">
       <div style={{ display: "flex" }}>
         <Menu />
-        {/* <MapWrapper /> */}
         <GoogleMap />
+        {/* <MapWrapper /> */}
       </div>
     </div>
   );
--- src/components/ggmap/GoogleMap.js
+++ src/components/ggmap/GoogleMap.js
@@ -1,16 +1,23 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { Menu } from "antd";
 import Map from "ol/Map.js";
 import View from "ol/View.js";
-import OLGoogleMaps from "olgm/OLGoogleMaps.js";
-import GoogleLayer from "olgm/layer/Google.js";
+import Overlay from "ol/Overlay.js";
 import { defaults as defaultInteractions } from "olgm/interaction.js";
 import { transform } from "ol/proj.js";
-import { Vector as VectorSource } from "ol/source.js";
-import { Tile as TileLayer, Vector as VectorLayer } from "ol/layer.js";
-import { Draw, Modify, Snap } from "ol/interaction.js";
+import { Tile as TileLayer } from "ol/layer.js";
 import XYZ from "ol/source/XYZ";
+import Draw from "ol/interaction/Draw";
+import { Vector as VectorLayer } from "ol/layer";
+import { Vector as VectorSource } from "ol/source";
+import { Stroke, Style } from "ol/style";
+import "./GoogleMap.css";
+
 const GoogleMap = () => {
+  const [drawEnabled, setDrawEnabled] = useState(false);
   const mapRef = useRef(null);
+  const drawRef = useRef(null);
+  const overlayRef = useRef(null);
 
   useEffect(() => {
     if (!mapRef.current) {
@@ -37,20 +44,97 @@ const GoogleMap = () => {
         }),
       });
 
+      // Create an overlay to display the length of the drawn line
+      const overlayElement = document.createElement("div");
+      overlayElement.className = "overlay";
+      const overlay = new Overlay({
+        element: overlayElement,
+        positioning: "bottom-center",
+      });
+      map.addOverlay(overlay);
+      overlayRef.current = overlay;
+
       mapRef.current = map;
+      // Create a vector layer to display the drawn features
+      const vectorLayer = new VectorLayer({
+        source: new VectorSource(),
+        style: new Style({
+          stroke: new Stroke({
+            color: "#ff0000",
+            width: 2,
+          }),
+        }),
+      });
+      // Add the vector layer to the map
+      map.addLayer(vectorLayer);
+      // Create a Draw interaction to draw lines on the map
+      const draw = new Draw({
+        type: "LineString",
+        source: vectorLayer.getSource(),
+      });
+      // Listen for the drawend event to calculate the length of the drawn line
+      draw.on("drawend", (event) => {
+        const feature = event.feature;
+        const geometry = feature.getGeometry();
+        const lengthInMeters = geometry.getLength();
+        console.log(`Length of drawn line: ${lengthInMeters} meters`);
+      });
+      drawRef.current = draw;
+
+      // Listen for pointermove events on the map to display the length of the drawn line
+      map.on("pointermove", (event) => {
+        if (event.dragging) {
+          return;
+        }
+        const pixel = map.getEventPixel(event.originalEvent);
+        const feature = map.forEachFeatureAtPixel(pixel, (feature) => feature);
+        if (feature) {
+          const geometry = feature.getGeometry();
+          if (geometry.getType() === "LineString") {
+            const lengthInMeters = geometry.getLength();
+            const lengthInKilometers = lengthInMeters / 1000;
+            overlayElement.innerHTML = `${lengthInKilometers.toFixed(2)} km`;
+            overlay.setPosition(event.coordinate);
+          }
+        } else {
+          overlay.setPosition(undefined);
+        }
+      });
     }
   }, []);
 
+  useEffect(() => {
+    if (drawEnabled) {
+      mapRef.current.addInteraction(drawRef.current);
+    } else {
+      mapRef.current.removeInteraction(drawRef.current);
+    }
+  }, [drawEnabled]);
+
   return (
-    <div id="map" style={{ height: "100vh", width: "80vw" }}>
-      <style>
-        {`
-          .ol-zoom, .ol-rotate-reset {
-            display: none !important;
-          }
-        `}
-      </style>
-    </div>
+    <>
+      <div id="map" style={{ height: "100vh", width: "100vw" }}>
+        <style>
+          {`
+            .ol-zoom, .ol-rotate-reset {
+              display: none !important;
+            }
+            
+            .overlay {
+              background-color: white;
+              border-radius: 4px;
+              padding: 4px 8px;
+              opacity: 0.8;
+            }
+          `}
+        </style>
+        <Menu mode="horizontal" className="menu">
+          <Menu.Item key="draw" onClick={() => setDrawEnabled(!drawEnabled)}>
+            {drawEnabled ? "Hủy vẽ" : "Nhấp để vẽ"}
+          </Menu.Item>
+        </Menu>
+      </div>
+    </>
   );
 };
 
